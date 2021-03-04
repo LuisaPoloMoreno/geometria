21b82ec8c66aec96bf5fc7c077715a576b3f7184
Luisa Fernanda Polo Moreno <luisita021194@gmail.com>
2021-03-04 11:10:05 +0100

    versión refactorizada

--- geometria/src/FiguraGeometrica_LFPM.java
+++ geometria/src/FiguraGeometrica_LFPM.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_LFPM {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_LFPM(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
